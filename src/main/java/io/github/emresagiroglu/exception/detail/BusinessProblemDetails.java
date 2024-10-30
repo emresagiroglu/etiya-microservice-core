06a98ad6084260b9c2a930f806dca63b1aead00b
Orhun Bayındır <orhunbayindir95@gmail.com>
2024-10-30 18:24:05 +0300

    Exception

---
+++ src/main/java/io/github/emresagiroglu/exception/detail/BusinessProblemDetails.java
@@ -0,0 +1,8 @@
+package io.github.emresagiroglu.exception.detail;
+
+public class BusinessProblemDetails extends ProblemDetails {
+  public BusinessProblemDetails(){
+    setType("");
+    setStatus("400");
+  }
+}
