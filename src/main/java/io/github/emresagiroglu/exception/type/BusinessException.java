483c02e31323112b49b027801d8c3cec85fdc5b8
emre.sagiroglu <emresagiroglu1@gmail.com>
2024-10-31 11:09:18 +0300

    Exception type updated

--- src/main/java/io/github/emresagiroglu/exception/type/BusinessException.java
+++ src/main/java/io/github/emresagiroglu/exception/type/BusinessException.java
@@ -1,5 +1,9 @@
 package io.github.emresagiroglu.exception.type;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class BusinessException extends RuntimeException{
     public BusinessException(String message) {
         super(message);
